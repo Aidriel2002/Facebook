aa61ea7f960e5491fa8f94b6e80144d5851fc1af
Aidriel2002 <147413687+Aidriel2002@users.noreply.github.com>
2024-08-18 13:56:30 +0000

    g

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('linktofb').onclick = function() {
-        window.location.href = 'https://www.facebook.com/login';
-    };
+
